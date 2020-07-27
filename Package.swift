c45ef73ab0df59654d59770383456d1df47484ad
Le Ngoc Duy <askbills@gmail.com>
2020-07-27 21:03:58 +0700

    chore(distribution): publish the framework via cocoapod and swift package manager

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.0
+
+import PackageDescription
+
+let package = Package(
+    name: "DLAnalytics",
+    products: [
+        .library(name: "DLAnalytics", targets: ["DLAnalytics"])
+    ],
+    targets: [
+        .target(
+            name: "DLAnalytics",
+            dependencies: [])
+    ]
+)
